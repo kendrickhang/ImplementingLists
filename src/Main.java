23fa555a50a00727e93b9eee3ca9828c72577813
AdamZWinter <adam@bootstrapmusic.com>
2023-01-11 14:29:08 -0800

    From CS145

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import edu.greenriver.sdev333.ArrayList;
+import edu.greenriver.sdev333.List;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        List<String> friends = new ArrayList<String>();
+        System.out.println("Initial size is: "+friends.size());
+
+        friends.add("Ehren");
+        System.out.println("Size after added one: "+friends.size());
+
     }
 }
--- src/edu/greenriver/sdev333/ArrayList.java
+++ src/edu/greenriver/sdev333/ArrayList.java
@@ -1,4 +1,308 @@
 package edu.greenriver.sdev333;
 
-public class ArrayList {
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class ArrayList<ItemType> implements List<ItemType>{
+
+    // one plain old java array
+    private ItemType[] data;
+
+    // one int to keep track of size
+    // size is the spots that are used in the data array
+    // size is different than lenth of the array
+    private int size;
+
+    public ArrayList() {
+        this.data = (ItemType[]) new Object[10];
+        this.size = 0;
+    }
+
+    /**
+     * Returns the number of items in this collection.
+     *
+     * @return the number of items in this collection
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns true if this collection contains no items.
+     *
+     * @return true if this collection contains no items
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Returns true if this collection contains the specified item.
+     *
+     * @param item items whose presence in this collection is to be tested
+     * @return true if this collection contains the specified item
+     * @throws NullPointerException if the specified item is null
+     *                              and this collection does not permit null items
+     */
+    @Override
+    public boolean contains(ItemType item) {
+        return false;
+    }
+
+    /**
+     * Returns an iterator over the elements in this collection.
+     *
+     * @return an Iterator over the elements in this collection
+     */
+    @Override
+    public Iterator<ItemType> iterator() {
+        return null;
+    }
+
+    /**
+     * Performs the given action for each element of the {@code Iterable}
+     * until all elements have been processed or the action throws an
+     * exception.  Actions are performed in the order of iteration, if that
+     * order is specified.  Exceptions thrown by the action are relayed to the
+     * caller.
+     * <p>
+     * The behavior of this method is unspecified if the action performs
+     * side-effects that modify the underlying source of elements, unless an
+     * overriding class has specified a concurrent modification policy.
+     *
+     * @param action The action to be performed for each element
+     * @throws NullPointerException if the specified action is null
+     * @implSpec <p>The default implementation behaves as if:
+     * <pre>{@code
+     *     for (T t : this)
+     *         action.accept(t);
+     * }</pre>
+     * @since 1.8
+     */
+    @Override
+    public void forEach(Consumer<? super ItemType> action) {
+        List.super.forEach(action);
+    }
+
+    /**
+     * Creates a {@link Spliterator} over the elements described by this
+     * {@code Iterable}.
+     *
+     * @return a {@code Spliterator} over the elements described by this
+     * {@code Iterable}.
+     * @implSpec The default implementation creates an
+     * <em><a href="../util/Spliterator.html#binding">early-binding</a></em>
+     * spliterator from the iterable's {@code Iterator}.  The spliterator
+     * inherits the <em>fail-fast</em> properties of the iterable's iterator.
+     * @implNote The default implementation should usually be overridden.  The
+     * spliterator returned by the default implementation has poor splitting
+     * capabilities, is unsized, and does not report any spliterator
+     * characteristics. Implementing classes can nearly always provide a
+     * better implementation.
+     * @since 1.8
+     */
+    @Override
+    public Spliterator<ItemType> spliterator() {
+        return List.super.spliterator();
+    }
+
+    /**
+     * Adds the specified item to the collection.
+     *
+     * @param item item to be added to the collection
+     * @throws NullPointerException if the specified item is null
+     *                              and this collection does not permit null items
+     */
+    @Override
+    public void add(ItemType item) {
+        if(size == data.length){
+            //resize up : double array size
+            ItemType[] temp = (ItemType[]) new Object[size * 2];
+            for(int i=0; i < this.size; i++){
+                temp[i] = this.data[i];
+            }
+            this.data = temp;
+            temp = null;  //optional : gone as soon as method closes anyway
+        }
+        data[size] = item;
+        size++;
+
+    }
+
+    /**
+     * Removes a single instance of the specified item from this collection,
+     * if it is present.
+     *
+     * @param item item to be removed from this collection, if present
+     * @throws NullPointerException if the specified item is null
+     *                              and this collection does not permit null items
+     */
+    @Override
+    public void remove(ItemType item) {
+
+    }
+
+    /**
+     * Removes all items from this collection.
+     * The collection will be empty after this method returns.
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    /**
+     * Returns true if this collection contains all the items
+     * in the specified other collection.
+     *
+     * @param otherCollection collection to be checked for containment in this collection
+     * @return true if this collection contains all the items
+     * in the specified other collection
+     */
+    @Override
+    public boolean containsAll(Collection<? extends ItemType> otherCollection) {
+        return false;
+    }
+
+    /**
+     * Adds all the items in this specified other collection to this collection.
+     *
+     * @param otherCollection collection containing items to be added to this collection
+     */
+    @Override
+    public void addAll(Collection<? extends ItemType> otherCollection) {
+
+    }
+
+    /**
+     * Removes all of this collection's items that are also contained in the
+     * specified other collection. After this call returns, this collection will
+     * contain no elements in common with the specified other collection.
+     *
+     * @param otherCollection collection containing elements to be removed
+     *                        from this collection
+     */
+    @Override
+    public void removeAll(Collection<? extends ItemType> otherCollection) {
+
+    }
+
+    /**
+     * Retains only the items in this collection that are contained in the
+     * specified other collection. In other words, removes from this collection
+     * all of its items that are not contained in the specified other collection
+     *
+     * @param otherCollection collection containing elements to be retained in
+     *                        this collection
+     */
+    @Override
+    public void retainAll(Collection<? extends ItemType> otherCollection) {
+
+    }
+
+    /**
+     * Returns the item at the specified position in this list
+     *
+     * @param index index of the item to return
+     * @return the item at the specified position in this list
+     * @throws IndexOutOfBoundsException if this index is out of range
+     *                                   (index < 0 || index >= size())
+     */
+    @Override
+    public ItemType get(int index) {
+        return null;
+    }
+
+    /**
+     * Replaces the item at the specified position in this list
+     * with the specified item
+     *
+     * @param index index of the item to replace
+     * @param item  item to be stored at the specified position
+     * @throws NullPointerException      if the specified item is null
+     *                                   and this list does not permit null elements
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (index < 0 || index >= size())
+     */
+    @Override
+    public void set(int index, ItemType item) {
+
+    }
+
+    /**
+     * Inserts the specified item at the specified position in this list.
+     * Shifts the item currently at that position (if any) and any subsequent
+     * items to the right.
+     *
+     * @param index index at which the specified item is to be inserted
+     * @param item  item to be inserted
+     * @throws NullPointerException      if the specified item is null
+     *                                   and this list does not permit null elements
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (index < 0 || index >= size())
+     */
+    @Override
+    public void add(int index, ItemType item) {
+        data[size] = item;
+        size++;
+    }
+
+    /**
+     * Removes the element at the specified position in this list.
+     * Shifts any subsequent items to the left.
+     *
+     * @param index the index of the item to be removed
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   (index < 0 || index >= size())
+     */
+    @Override
+    public void remove(int index) {
+
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified item
+     * in this list, or -1 if this list does not contain the item.
+     *
+     * @param item the item to search for
+     * @return the index of the first occurrence of the specified item
+     * in this list, or -1 if this list does not contain the item
+     * @throws NullPointerException if the specified item is null and this
+     *                              list does not permit null items
+     */
+    @Override
+    public int indexOf(ItemType item) {
+        return 0;
+    }
+
+    /**
+     * Returns the index of the last occurrence of the specified item
+     * in this list, or -1 if this list does not contain the item.
+     *
+     * @param item the item to search for
+     * @return the index of the first occurrence of the specified item
+     * in this list, or -1 if this list does not contain the item
+     * @throws NullPointerException if the specified item is null and this
+     *                              list does not permit null items
+     */
+    @Override
+    public int lastIndexOf(ItemType item) {
+        return 0;
+    }
+
+    /**
+     * Returns a list iterator over the elements in this list
+     * (in proper sequence).
+     *
+     * @return a list iterator over the elements in this list
+     * (in proper sequence)
+     */
+    @Override
+    public ListIterator<ItemType> listIterator() {
+        return null;
+    }
 }
