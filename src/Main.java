68f29ea625e0f1925da1749522f2bf3b0510effc
Kendrick Hang <kendrickhang@users.noreply.github.com>
2023-01-11 14:30:34 -0800

    Implemented the constructor, size, add.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import edu.greenriver.sdev333.*;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        List<String> friends = new ArrayList<String>();
+        System.out.println("initial size is " + friends.size());
+
+        friends.add("Jess");
+        friends.add("Tina");
+        friends.add("Josh");
+        friends.add("Susan");
+        friends.add("Tyler");
+        friends.add("Usman");
+        friends.add("Dee");
+        friends.add("Rose");
+        friends.add("Blanche");
+        friends.add("Dorothy");
+        friends.add("Sophia");
+        System.out.println("size is now " + friends.size());
+
     }
 }
